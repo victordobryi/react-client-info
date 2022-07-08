a14e603f07c86b12428c5bc05072bd690ff523fa
bazla <dobr1nya@mail.ru>
2022-07-08 09:18:55 +0300

    init: start react-client-info task

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+import './styles/main.scss';
+
+export const App = () => (
+  <Fragment>
+    <h1>React TypeScript Webpack Starter Template</h1>
+  </Fragment>
+);
+
+export default App;
